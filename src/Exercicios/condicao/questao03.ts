e84648f82200ee58112103e0527ef91161f74a5a
Daniel_Alonso <falecomdanielramos@gmail.com>
2026-04-10 15:58:27 -0300

    Create questao03.ts
    mais 1 feito

---
+++ src/Exercicios/condicao/questao03.ts
@@ -0,0 +1,33 @@
+// 3. Crie um programa que solicite dois números e simule um menu de uma calculadora:
+// 1 - Soma
+// 2 - Subtração
+// 3 - Multiplicação
+// 4 - Divisão
+// Use switch...Case
+
+let mensagem:number;
+let nu1:number = Number(prompt("Informe um numero: "))
+let nu2:number = Number(prompt("Iforme outro numero: "))
+
+
+let op:string = String(prompt("Informe a operação: soma,subtração,multiplicação,divisão")).toLowerCase()
+
+
+switch (op){
+    case "soma":
+        mensagem = nu1 + nu2;
+    break;
+
+    case "subração":
+        mensagem = nu1 - nu2;
+    break;
+
+    case "multiplicação":
+        mensagem = nu1 * nu2;
+    break;
+
+    case "divisão":
+        mensagem = nu1 / nu2;
+    break;
+}
+console.log(mensagem);
