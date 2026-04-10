9db4d2e03ff49795d2a85801eedca949a5c1de7d
Daniel_Alonso <falecomdanielramos@gmail.com>
2026-04-10 14:55:52 -0300

    Questão

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-// 2. Crie uma função que receba um número e retorne se ele é par ou ímpar.
-
+// 1. Crie um programa que leia um número e informe se ele é:
+//  Par ou Ímpar
+//  Positivo ou Negativo
 
 
 function digito(){
-    let numero = Number(prompt("Informe um numero: "))
-    let impar = numero%2 == 0
+    let numero:number = Number(prompt("Informe um numero: "))
 
-
-
-
+    if(numero%2 == 0){
+        console.log("Esse numero e Impar: "+numero)
+}else{
+    console.log("Esse numero e Par: "+numero)
+}
 }
