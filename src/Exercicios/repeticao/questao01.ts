083ca47531c0c741a7eca697682f8b64f614b2c5
Anonimo <falecomdanielramos@gmail.com>
2026-04-15 09:12:51 -0300

    Create questao01.ts

---
+++ src/Exercicios/repeticao/questao01.ts
@@ -0,0 +1,20 @@
+// 1. Crie um programa que peça números até o usuário digitar 0 e mostre:
+//  Quantidade de números digitados
+//  Soma total
+
+
+let soma= 0,quanti:=0
+
+
+while(true){
+        let numero:number = Number(prompt("Informe os números {0 para sair}"))
+
+        if(numero == 0){
+            break
+        }
+
+        quanti++
+        soma = soma + numero
+}
+console.log(`A Quantidade dos números foi: ${quanti}`)
+console.log(`A Soma dos números foi: ${soma}`)
