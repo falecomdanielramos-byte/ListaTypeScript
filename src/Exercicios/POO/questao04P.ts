f6235dd28d7fbb7353b455a9e969b772d34450ad
Daniel_Alonso <falecomdanielramos@gmail.com>
2026-06-03 17:56:38 -0300

    Atualizar o questao04P.ts

---
+++ src/Exercicios/POO/questao04P.ts
@@ -0,0 +1,20 @@
+//4. Crie um programa que utilize a classe acima. Ele deve pedir ao usuário que informe as medidas de um
+//local. Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés
+//necessárias para o local.
+
+
+class retengulo{
+
+Base:number
+Altura:number
+
+constructor(B:number,A:number){
+this.Base=B
+this.Altura=A
+}
+
+Exigir():void{
+window.alert(`A `)
+}
+
+}
