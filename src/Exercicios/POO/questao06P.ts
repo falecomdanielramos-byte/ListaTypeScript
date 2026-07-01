61855aa1c1c21960837726933495b0808394d7ca
LaraSantana27 <falecomdanielramos@gmail.com>
2026-07-01 09:31:00 -0300

    Update questao06P.ts

--- src/Exercicios/POO/questao06P.ts
+++ src/Exercicios/POO/questao06P.ts
@@ -18,7 +18,6 @@ class Conta {
     ){
         this.Numero_Conta=Nu
         this.Nome_Cnta=No
-        this.Saldo
     }
 
     AlterarNome(NovoNome:string){
@@ -31,11 +30,12 @@ class Conta {
         this.Saldo = NovoSaldo
     }
     Saque(RemoverSal:number){
-        RemoverSal = saldo
+        RemoverSal = saldo - saque
+        this.Saldo = RemoverSal
     }
 
     Exibir():void{
-        window.alert()
+        window.alert(`A Conta do Usúario: ${this.Nome_Cnta} | Possui o Numero: ${this.Numero_Conta} | Com o Saldo de: ${this.Saldo}`)
     }
 
 }
@@ -44,27 +44,23 @@ let conta:number,nome:string,saldo:number
 let continuar = "",saquar:string,saque:number
 
 while(continuar === "S"|| continuar === "SIM"){
+
 conta = Number(prompt("Informe o número da conta: "))
 nome = String(prompt("Informe o nome do Correntista: "))
 saldo = Number(prompt("Informe o Saldo da conta: "))
 continuar = String(prompt("Quer continuar? S-Sim ou N-Não")).toLocaleUpperCase()
-saquer = String(prompt("Quer Saquer ? S-Sim ou N-Não: ")).toLocaleUpperCase
+saquar = String(prompt("Quer Saquer ? S-Sim ou N-Não: ")).toLocaleUpperCase()
 
 if(saquar === "S"){
-    
+    saque = Number(prompt("Informe o Valor do saque: "))
 }
 
 let ListaConta = []
-let novaConta = new Conta (conta,nome)
+let novaConta = new Conta (conta,nome,)
 
 ListaConta.push(novaConta)
 
 for(let novaConta of ListaConta){
-
+    novaConta.Exibir
 }
 }
-
-
-
-
-
