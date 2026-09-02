09c9f1a45e4d58f096d880ac134b558781b82bf0
anonimo <falecomdanielramos@gmail.com>
2026-09-02 09:46:57 -0300

    Update questao07P.ts

--- src/Exercicios/POO/questao07P.ts
+++ src/Exercicios/POO/questao07P.ts
@@ -11,24 +11,50 @@ class Funcionario{
 
     nome:string
     cargo:string
+    percent:number
     salario:number=1700
 
     constructor(
         No:string,
         Ca:string,
+        Pc:number
         
     ){
         this.nome=No
         this.cargo=Ca
+        this.percent=Pc
         
     }
 
 
-ADDper(){
-
+percentAL(){
+    let valorPerc:number = this.salario + (this.salario * percent)/100
 }
 
 
+}
+
+let nome:string,cargo:string,percent:number
+let continuar = ""
+
+continuar = String(prompt("Quer calcular o salario ? (S-Sim ou N-Não)")).toUpperCase()
+
+while(continuar != "SIM"){
+    nome = String(prompt("Informe o nome do funcionario: "))
+    cargo = String(prompt("Informe o cargo do funcionario: "))
+    percent = Number(prompt("Informe o percentual do aumento: "))
+
+    let ListaFunc = []
+    
+    continuar = String(prompt("Deseja continuar ? (S-Sim ou N-Não)")).toUpperCase()
+
+    let funcionario:Funcionario = new Funcionario(nome,cargo,percent)
+
+    ListaFunc.push(funcionario)
+
+for(let funcionario of ListaFunc){
+
+}
 }
 
 
