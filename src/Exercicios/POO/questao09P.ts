96367edf2599b04f42110de20fdf567474d42de1
anonimo <falecomdanielramos@gmail.com>
2026-09-09 09:55:34 -0300

    Update questao09P.ts

--- src/Exercicios/POO/questao09P.ts
+++ src/Exercicios/POO/questao09P.ts
@@ -4,22 +4,68 @@
 // cada produto.
 
 
-export function questao09P():void{
+export function questao09P(): void {
+
+    class Loja {
+
+        nome: string
+        produto: string
+        preco: number
+        quantidade: number
 
 
 
+        constructor(
+            No: string,
+            Prod: string,
+            Preço: number,
+            quant: number
+
+        ) {
+            this.nome = No
+            this.produto = Prod
+            this.preco = Preço
+            this.quantidade = quant
+        }
+
+        Exibirestoque() {
+            let estoque = this.preco * this.quantidade
+
+            window.alert(`O cliente: ${this.nome} | comprou: ${this.produto} | com o valor de: ${this.preco.toFixed(2)} | na quantidade: ${this.quantidade.toFixed(2)} || Valor total em estoque: ${estoque.toFixed(2)}`)
+        }
+
+    }
+
+
+    let nome: string, produto: string, preco: number, quantidade: number
+    let continuar = ""
+    let ListaProdu: Loja[] = []
+
+    continuar = String(prompt("Deseja comprar? (S-Sim ou N-Não)")).toUpperCase()
+
+    while (continuar != "N") {
+
+
+        nome = String(prompt("Informe o nome do usuario: "))
+        produto = String(prompt("Informe o nome do produto: "))
+        preco = Number(prompt("Informe o preço do produto: "))
+        quantidade = Number(prompt("Informe a quantidade do produto: "))
+        continuar = String(prompt("Deseja continuar ? (S-Sim ou N-Não)")).toUpperCase()
+
+
+        let produ: Loja = new Loja(nome, produto, preco, quantidade)
+
+        ListaProdu.push(produ)
 
 
 
+    }
+
+    for (let produ of ListaProdu) {
+        produ.Exibirestoque()
+
+    }
 
 
 
-
-
-
-
-
-
-
-    
 }
