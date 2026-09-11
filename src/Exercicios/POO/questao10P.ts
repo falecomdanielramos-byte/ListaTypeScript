e3b8de0d9597416a02bb2bfba47eb06edd2252d2
anonimo <falecomdanielramos@gmail.com>
2026-09-11 15:48:38 -0300

    Update questao10P.ts

--- src/Exercicios/POO/questao10P.ts
+++ src/Exercicios/POO/questao10P.ts
@@ -26,38 +26,80 @@ class Bichinho_Virtual{
     }
 
 
-    AlterarNome(){}
-    Fome(){}
-    Saude(){}
-    Idade(){}
+    AlterarNome(NovoNome:string){
+        this.nome=NovoNome
+    }
+    AlterarFome(NovaFome:number){
+        this.fome=NovaFome
+    }
+    AlterarSaude(NovaSaude:number){
+        this.saude=NovaSaude
+    }
+    AlterarIdade(NovaIdade:number){
+        this.idade=NovaIdade
+    }
+    Exibir():void{
+        window.alert(`O Tamagushi: ${this.nome} | Possui a fome: ${this.fome} | Possui a saude: ${this.saude} | Pessui a idade: ${this.idade}`)
+    }
+
+
 
 
 
 }
 
 let nome:string,fome:number,saude:number,idade:number
-let continuar=""
-continuar = String(prompt("Deseja iniciar ? (S-Sim ou N-Não)"))
+let continuar="",alter:number,ListaBichin:Bichinho_Virtual [] = []
 
-while( continuar != "N"){
+
+while(continuar != "N"){
+
+    
 
     nome = String(prompt("Informe o nome do Tamagushi: "))
     fome = Number(prompt("Informe a fome do Tamagushi: (1 a 10): "))
     saude = Number(prompt("Informe a saude do Tamagushi: (1 a 10): "))
     idade = Number(prompt("Informe a idade do Tamagushi: "))
+
+
+    let Bichinho = new Bichinho_Virtual(nome,fome,saude,idade)
+
+    ListaBichin.push(Bichinho)
+
+    continuar = String(prompt("Deseja alterar? (S-Sim ou N-Não)"))
+
+    if(continuar === "N"){
+        break
+    }
+
+    else if(continuar === "S"){
+
+    alter = Number(prompt("informe se deseja mudar algo: ( Alterar \n (1-Nome) \n (2-fome) \n (3-saude) \n (4-idade) )"))
+
+    if(alter === 1){
+        let NovoNome = String(prompt("Informe qual o novo nome do Tamagushi: "))
+        Bichinho.AlterarNome(NovoNome)
+    }
+    else if(alter === 2){
+        let NovaFome = Number(prompt("Informe qual e a nova fome do Tamagushi: "))
+        Bichinho.AlterarFome(NovaFome)
+    }
+    else if(alter === 3){
+        let NovaSaude = Number(prompt("Informe qual e a nova saude do Tamagushi: "))
+        Bichinho.AlterarSaude(NovaSaude)
+    }
+    else if(alter === 4){
+        let NovaIdade = Number(prompt("Informe qual e a nova idade do Tamagushi: "))
+        Bichinho.AlterarIdade(NovaIdade)
+    }
+    else if(alter >=5 || alter <=0){ 
+        window.alert(`INFORMAÇÂO INVALIDA....tente novamente.`)
+        
+    }
+    }
+   
     continuar = String(prompt("Deseja continuar? "))
-
-
-
-
-    
-
 }
-
-
-
-
-
 
 
 }
