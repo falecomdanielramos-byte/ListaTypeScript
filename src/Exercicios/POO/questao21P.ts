032b722889509c8dd48fde20c963f78e82da7903
anonimo <falecomdanielramos@gmail.com>
2026-09-16 10:25:46 -0300

    Create questao21P.ts

---
+++ src/Exercicios/POO/questao21P.ts
@@ -0,0 +1,61 @@
+// 21. Abstração Herança Polimorfismo Repetição Encapsulamento Arrays
+// Concurso de Projetos de Extensão Reforest
+// O projeto socioambiental &quot;Flor&amp;Ser&quot; abriu inscrições para novas propostas de reflorestamento no
+// campus. Cada projeto inscrito possui título, coordenador e uma nota de avaliação avaliada de forma
+// estrita (protegida por métodos de validação para que não receba valores fora do intervalo de 0 a 10).
+// Existem Projetos Verdes (focados em plantio urbano) e Projetos Culturais (focados em
+// conscientização). O usuário deve preencher a lista de projetos avaliados através do terminal. O
+// programa deve calcular a média aritmética de todas as notas usando estruturas de array e, em seguida,
+// listar de forma inversa à inscrição quais projetos ganharam nota acima da média da competição.
+
+
+
+ export function questao21P():void{
+
+
+class FloreSer {
+    titulo:string
+    coordendor:string
+    private _nota:number
+
+
+    constructor(
+        Tit:string,
+        Coop:string,
+        No:number
+
+    ){
+        this.titulo=Tit
+        this.coordendor=Coop
+        this._nota=No
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
